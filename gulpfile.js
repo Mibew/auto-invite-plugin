f3519bedba1fb9c47da836837b4285684b3ac65a
Fedor A. Fetisov <faf@ossg.ru>
2021-01-30 00:03:33 +0300

    Switch go Gulp 4 for builds

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var eventStream = require('event-stream'),
     gzip = require('gulp-gzip'),
     rename = require('gulp-rename');
 
-gulp.task('prepare-release', [], function() {
+gulp.task('prepare-release', function() {
     var version = require('./package.json').version;
 
     return eventStream.merge(
@@ -21,9 +21,7 @@ gulp.task('prepare-release', [], function() {
 });
 
 // Builds and packs plugins sources
-gulp.task('default', ['prepare-release'], function() {
-    // The "default" task is just an alias for "prepare-release" task.
-});
+gulp.task('default', gulp.series('prepare-release'));
 
 /**
  * Returns files stream with the plugin sources.
